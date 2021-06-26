db007ba049a74fba1dabad869d3d644fc77fbd62
MarcoMartiniano <79107727+MarcoMartiniano@users.noreply.github.com>
2021-06-26 09:57:23 -0300

    commit3
    imports

--- app/src/main/java/com/plcoding/jetpackcomposepokedex/pokemondetail/pokemonDetailScreen.kt
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/pokemondetail/pokemonDetailScreen.kt
@@ -10,6 +10,8 @@ import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import com.plcoding.jetpackcomposepokedex.R
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
